27cba4ec661895f084ce09dc85a652af0d8ee5e0
Alvin <hello@alvin.codes>
2023-03-14 14:59:59 +0000

    👨‍🏫 291!

--- examples/291/main.rs
+++ examples/291/main.rs
@@ -1,17 +1,32 @@
-fn str_to_frac(string: &str) -> f64 {
-    let operands: Vec<&str> = string.split("/").collect();
+use fraction::Fraction;
+
+fn frac_to_num(frac: &str) -> f64 {
+    let operands: Vec<&str> = frac.split("/").collect();
     let a: f64 = operands[0].parse().unwrap();
     let b: f64 = operands[1].parse().unwrap();
-
     return a / b;
 }
 
-fn fraction_math(a: &str, operation: &str, b: &str) {
-    let frac_a = str_to_frac(a);
-    println!("{}", frac_a);
+fn do_operation(a: f64, b: f64, operation: &str) -> f64 {
+    let result = match operation {
+        "add" => a + b,
+        "subtract" => a - b,
+        "multiply" => a * b,
+        "divide" => a / b,
+        &_ => a + b,
+    };
+
+    return result;
+}
+
+fn fraction_math(a: &str, operation: &str, b: &str) -> String {
+    let num_a = frac_to_num(a);
+    let num_b = frac_to_num(a);
+    let num_result = do_operation(num_a, num_b, operation);
+    return format!("{}", Fraction::from(num_result));
 }
 
 fn main() {
-    println!("{}", "hi");
     let r = fraction_math("3/4", "add", "3/4");
+    println!("{}", r);
 }
