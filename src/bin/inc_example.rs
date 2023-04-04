2b2038c856c6d00d84d89016a9afe95083014a12
Alvin <hello@alvin.codes>
2023-04-04 21:54:01 +0100

    making progress on the script

--- src/bin/inc_example.rs
+++ src/bin/inc_example.rs
@@ -3,14 +3,25 @@ use std::fs;
 fn get_latest_dir() -> i32 {
     let paths = fs::read_dir("./examples").unwrap();
     let mut greatest = 0;
+
     for path in paths {
-        println!("Name: {:#?}", path.unwrap());
+        let p = path.unwrap().path();
+        if p.is_dir() {
+            let dir_name: String = p.to_str().unwrap().to_owned();
+            let top_level = dir_name.split("/").last().unwrap();
+            let top_level_num: i32 = top_level.parse().unwrap();
+
+            if top_level_num > greatest {
+                greatest = top_level_num;
+            }
+        }
     }
 
-    return 2;
+    return greatest;
 }
 
 fn main() {
     let latest = get_latest_dir();
-    println!("{}", latest);
+    let new_dir = format!("./examples/{}", latest + 1);
+    let new_file = format!("{}/main.rs", new_dir);
 }
